5f909ec912ab30638be42d16db2dcfee7d3785cd
war <37655074@qq.com>
2019-01-07 20:54:15 +0800

    服务超时 update

--- server/src/main/java/com/imooc/order/controller/HystrixController.java
+++ server/src/main/java/com/imooc/order/controller/HystrixController.java
@@ -2,6 +2,7 @@ package com.imooc.order.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +16,10 @@ import java.util.Arrays;
 @DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
 
-    @HystrixCommand
+    //设置超时时间为3秒
+    @HystrixCommand(commandProperties = {
+        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     @GetMapping("/getProductInfoList")
     public String getProductInfoList(){
         RestTemplate restTemplate = new RestTemplate();
